1f618e5bce22ea4fb84bc971fc172fb1517af687
Yushan <usndangol972gmail.com>
2021-04-24 14:49:15 +0545

    First Commit

---
+++ ExpenseTrackerStarter/app/src/main/java/com/example/expensetrackerstarter/TransFragment.kt
@@ -0,0 +1,31 @@
+package com.example.expensetrackerstarter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class TransFragment : Fragment() {
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trans, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.floating_action_button).setOnClickListener{
+            navController!!.navigate(R.id.action_transFragment_to_formFragment)
+        }
+    }
+}
